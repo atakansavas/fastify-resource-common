4b0bb8c7e3f1ac246ef572e56688c4ed14c6bcdc
Atakan Savas <a.savas@hotmail.com.tr>
2020-12-28 01:04:54 +0300

    enums updated

--- enums/index.js
+++ enums/index.js
@@ -1,5 +1,7 @@
 const Groups = require('./settingGroups');
 
 const System = require('./systemSettings');
+const PackageTypes = require('./packageTypes');
+const VehicleTypes = require('./vehicleTypes');
 
-module.exports = { Groups, System };
+module.exports = { Groups, System, PackageTypes, VehicleTypes };
---
+++ enums/packageTypes.js
@@ -0,0 +1,10 @@
+const Enum = require('../base/enum/index');
+
+const SettingGroups = new Enum({
+  Zarf: '1',
+  Paket: '2',
+  Koli: '3',
+  Ozel: '4',
+});
+
+module.exports = SettingGroups;
