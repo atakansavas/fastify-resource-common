488a658144e840be09c309effe56979c81670daa
Atakan Savas <a.savas@hotmail.com.tr>
2020-12-28 22:23:06 +0300

    settings updated

--- helpers/settingsHelper.js
+++ helpers/settingsHelper.js
@@ -2,11 +2,11 @@ const settingsHelper = {
   getGroupSchema: (groupId) => {
     switch (groupId) {
       case 1:
-        return require('../settings/system/schema');
+        return require('../settings/system/schema').settingSchema;
       case 2:
-        return require('../settings/system/schema');
+        return require('../settings/system/schema').settingSchema;
       case 3:
-        return require('../settings/system/schema');
+        return require('../settings/system/schema').settingSchema;
       default:
         return null;
         break;
