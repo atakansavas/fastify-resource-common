3ac13c1ff86acb89e5b2e3406d2c2c52f7794a2f
Atakan Savas <a.savas@hotmail.com.tr>
2021-01-09 20:28:26 +0300

    schema updated

--- enums/settings/packageTypes.js
+++ enums/settings/packageTypes.js
@@ -4,7 +4,13 @@ const SettingGroups = new Enum({
   Zarf: '1',
   Paket: '2',
   Koli: '3',
-  Ozel: '4',
+  Ozel1: '4',
+  Ozel2: '5',
+  Ozel3: '6',
+  Ozel4: '7',
+  Ozel5: '8',
+  Ozel6: '9',
+  Ozel7: '10',
 });
 
 module.exports = SettingGroups;
