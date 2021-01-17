5af1cb0cc555263b14c88978a2e24acafc6cb719
Atakan Savas <a.savas@hotmail.com.tr>
2021-01-17 23:03:49 +0300

    post method updated

--- base/api/frApi.js
+++ base/api/frApi.js
@@ -209,10 +209,6 @@ module.exports = class FrApi {
           schema: {
             tags: [this.tableName],
             body: this.clearSchemas(createValidationSchema),
-            // body: createValidationSchema,
-            response: {
-              201: createValidationSchema,
-            },
           },
         },
         async (request, reply) => {
