62a12c243dbd41b1e6b5d39a2ad4d1337ccaa373
Atakan Savas <a.savas@hotmail.com.tr>
2021-02-01 14:01:09 +0300

    tokenTypes added

--- enums/tokenTypes.js
+++ enums/tokenTypes.js
@@ -3,18 +3,18 @@ const Enum = require('enum');
 module.exports = new Enum({
   User: {
     Id: 1,
-    TokenExpireDayCount: 30,
+    TokenExpireDayCount: '30d',
   },
   Courier: {
     Id: 2,
-    TokenExpireDayCount: 30,
+    TokenExpireDayCount: '30d',
   },
   Admin: {
     Id: 3,
-    TokenExpireDayCount: 5,
+    TokenExpireDayCount: '5d',
   },
   Internal: {
     Id: 4,
-    TokenExpireDayCount: 5,
+    TokenExpireDayCount: '5d',
   },
 });
