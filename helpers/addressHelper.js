2f5e5319d32e224ae12e1c9653bb00f56fbe7a2a
Atakan Savas <a.savas@hotmail.com.tr>
2021-02-16 22:51:48 +0300

    address helpers added

---
+++ helpers/addressHelper.js
@@ -0,0 +1,47 @@
+const addressHelper = {
+  getAddressFormattedText: (address) => {
+    const city = address.city;
+    const county = address.county;
+    const district = address.district;
+    const postalCode = address.postalCode;
+    const street = address.street;
+    const buildingNo = address.buildingNo;
+    const floor = address.floor;
+    const flatNo = address.flatNo;
+
+    let response = '';
+    if (district) {
+      response += district + ' Mahallesi';
+    }
+
+    if (street) {
+      response += ' ' + street;
+    }
+
+    if (buildingNo) {
+      response += ' No:' + buildingNo;
+    }
+
+    if (floor) {
+      response += ' Kat:' + floor;
+    }
+
+    if (flatNo) {
+      response += ' Daire:' + flatNo;
+    }
+
+    if (county && city) {
+      response += ' ' + county + '/' + city;
+    }
+
+    return response;
+  },
+
+  getLoginQuery: (params) => {
+    return {
+      phoneNumber: params.phoneNumber,
+    };
+  },
+};
+
+module.exports = addressHelper;
--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,12 @@ const RequestHelper = require('./requestHelper');
 const Utilities = require('./utilities');
 const ValidationHelper = require('./validationHelper');
 const SettingsHelper = require('./settingsHelper');
+const AddressHelper = require('./addressHelper');
 
 module.exports = {
   RequestHelper,
   Utilities,
   ValidationHelper,
   SettingsHelper,
+  AddressHelper,
 };
