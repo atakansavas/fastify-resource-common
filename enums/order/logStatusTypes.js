ca835bdfecb52c7200ebc1e64c4a8b839cc70096
Atakan Savas <a.savas@hotmail.com.tr>
2021-02-23 22:16:13 +0300

    logs updated

--- enums/order/logStatusTypes.js
+++ enums/order/logStatusTypes.js
@@ -4,25 +4,31 @@ module.exports = new Enum({
   Assigned: {
     Id: 1,
     Desc: 'Kuryeye atandı.',
+    OrderStatusId: 1,
   },
   Picked: {
     Id: 2,
     Desc: 'Teslim alindi.',
+    OrderStatusId: 1,
   },
   Departed: {
     Id: 3,
     Desc: 'Kurye dağıtıma çıktı.',
+    OrderStatusId: 1,
   },
   Transfered: {
-    Id: 3,
+    Id: 4,
     Desc: 'Paket transfer edildi.',
+    OrderStatusId: 1,
   },
   Succeed: {
-    Id: 4,
+    Id: 5,
     Desc: 'Gönderim tamamlandı.',
+    OrderStatusId: 1,
   },
   Fail: {
-    Id: 5,
+    Id: 6,
     Desc: 'Gönderim tamamlanamadı.',
+    OrderStatusId: 1,
   },
 });
