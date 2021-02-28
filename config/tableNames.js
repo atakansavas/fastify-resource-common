117307426d22ec68cc250b427b73410dffd90e26
Atakan Savas <a.savas@hotmail.com.tr>
2021-02-28 18:12:52 +0300

    table names updated

--- config/tableNames.js
+++ config/tableNames.js
@@ -19,6 +19,8 @@ const tableNames = {
   Promotions: 'promotions_db',
   BalanceDb: 'balance_db',
   OrderLogsDb: 'orderlogs_db',
+  RequestDb: 'request_db',
+  NotificationsDb: 'notifications_db',
 };
 
 module.exports = tableNames;
