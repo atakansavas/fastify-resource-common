34bfae42eaf60cb383ff9916f9cd712c30d1fb7e
Atakan Savas <a.savas@hotmail.com.tr>
2021-03-08 23:43:42 +0300

    paymentProcesstypes added

--- enums/payments/balanceProcessTypes.js
+++ enums/payments/balanceProcessTypes.js
@@ -1,10 +1,54 @@
 const Enum = require('enum');
 
 module.exports = new Enum({
-  Income: {
+  CreditCardIncome: {
     Id: 1,
+    Desc: 'Kredi Karti Tahsilat',
+    IsIncome: true,
   },
-  Expense: {
+  BankIncome: {
     Id: 2,
+    Desc: 'Banka Tahsilat',
+    IsIncome: true,
+  },
+  CashIncome: {
+    Id: 3,
+    Desc: 'Nakit Tahsilat',
+    IsIncome: true,
+  },
+  BonusIncome: {
+    Id: 4,
+    Desc: 'Bonus / Prim',
+    IsIncome: true,
+  },
+  DeservedIncome: {
+    Id: 5,
+    Desc: 'Hakedis',
+    IsIncome: true,
+  },
+  CreditCardExpense: {
+    Id: 6,
+    Desc: 'Kredi Karti Ödeme',
+    IsIncome: false,
+  },
+  BankExpense: {
+    Id: 7,
+    Desc: 'Banka Ödeme',
+    IsIncome: false,
+  },
+  CashExpense: {
+    Id: 8,
+    Desc: 'Nakit Ödeme',
+    IsIncome: false,
+  },
+  PenalExpense: {
+    Id: 9,
+    Desc: 'Cezai Ödeme',
+    IsIncome: false,
+  },
+  Order: {
+    Id: 10,
+    Desc: 'Sipariş',
+    IsIncome: false,
   },
 });
